c65fc79f633ebfe499d9ba1aa6ff8c5d21d7c74c
Erica Cuthbert <B00162515@mytudublin.ie>
2026-08-02 23:26:00 +0100

    choir events added

--- db.php
+++ db.php
@@ -11,7 +11,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-echo "Database connected successfully";
 
 
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+
+include 'db.php';
+
+$sql = "SELECT * FROM events";
+
+$result = $conn->query($sql);
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -7,10 +17,27 @@
 
 <body>
 
-    <h1>Choir</h1>
+    <h1>Church Choir</h1>
 
-    <p>Welcome to the choir.</p>
+    <h2>Upcoming events</h2>
 
+    <?php while ($row = $result->fetch_assoc()) { ?>
+    <!-- https://www.php.net/manual/en/mysqli-result.fetch-row.php?  -->
+     <h3><?php echo $row["event_name"]; ?></h3>
+     
+     <h3><?php echo $row["event_date"]; ?></h3>
+
+     <h3><?php echo $row["location"]; ?></h3>
+
+    <?php
+
+include 'db.php';
+
+$sql = "SELECT * FROM events";
+
+$result = $conn->query($sql);
+
+?>
 </body>
 
 </html>
