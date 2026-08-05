9715e750a27935a494cc276efe0f9e1ccf1cc67e
Erica Cuthbert <B00162515@mytudublin.ie>
2026-08-05 23:26:42 +0100

    event.php HTML code updated

--- event.php
+++ event.php
@@ -1,10 +1,17 @@
 <HTML>
 	<BODY>
 		<FORM method="POST" action="event.php">
+
+			<label>Event Name:</label> 
 			<input type="text" name="event_name"><br>
+
+			<label>Event Date:</label>
 			<input type="date" name="event_date"><br>
+
+			<label>Event Location:</label>
 			<input type="text" name="event_location"><br>
-			<input type="submit"><br>
+
+			<input type="submit" value="Add Event"><br>
 		</FORM>
 	
 	</BODY>
