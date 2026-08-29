765292337007d3603ca37d45a3d6bc628a82e418
Erica Cuthbert <B00162515@mytudublin.ie>
2026-08-30 00:01:31 +0100

    log out works

---
+++ songList.php
@@ -0,0 +1,43 @@
+<?php
+
+session_start();
+include 'db.php';
+
+if (!isset($_SESSION["user_id"])) {
+    header("Location: login.php");
+    exit;
+}
+
+$result = $conn->query("SELECT * FROM songs");
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Songs</title>
+</head>
+
+<body>
+
+<h1>Choir Songs</h1>
+
+<?php while ($row = $result->fetch_assoc()) { ?>
+
+    <h3><?php echo $row["song_name"]; ?></h3>
+
+    <p href="view_song.php?id=<?php echo $row["song_id"]; ?>">
+        View Song </p>
+
+<?php } ?>
+
+<br><br>
+
+<a href="logout.php">Logout</a>
+
+</body>
+</html>
+
+<!-- https://www.php.net/manual/en/mysqli.prepare.php? -->
+<!-- https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/audio? -->
---
+++ songsDowloads.php
@@ -0,0 +1,3 @@
+<!-- https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/audio -->
+<!-- https://stackoverflow.com/questions/21386537/downloading-mp3-and-mp4-files-using-php -->
+<!-- https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/ -->
